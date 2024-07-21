94fc157ad907e74c736b60452c3f4df8308228e7
Mohamed <mohamed.ahmed5828@gmail.com>
2024-07-21 20:25:12 +0300

    done the select and working updateTable

--- code/tables/updateTable.sh
+++ code/tables/updateTable.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+source ./utils/helperFunctions
+
+updateTableContent() {
+    local schema="$1"
+    local table="$2"
+    local matchColumn="$3"
+    local matchValue="$4"
+    shift 4
+    local columns=("$@")
+
+    local tableFile="$DB_ROOT/$schema/$table"
+    local header
+    local dataTypes
+    local pkConstraints
+    local tmpFile
+
+    tmpFile=$(mktemp)
+    header=$(head -n 1 "$tableFile")
+    dataTypes=$(sed -n '2p' "$tableFile")
+    pkConstraints=$(sed -n '3p' "$tableFile")
+}
